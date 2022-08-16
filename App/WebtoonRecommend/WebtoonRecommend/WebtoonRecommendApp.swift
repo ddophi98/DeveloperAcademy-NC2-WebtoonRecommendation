928ff250fba8d66521758323455d23fac664f104
ddophi98 <lightcloud98@gmail.com>
2022-08-16 15:32:41 +0900

    xcode 프로젝트 추가

---
+++ App/WebtoonRecommend/WebtoonRecommend/WebtoonRecommendApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebtoonRecommendApp.swift
+//  WebtoonRecommend
+//
+//  Created by 김동락 on 2022/08/16.
+//
+
+import SwiftUI
+
+@main
+struct WebtoonRecommendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
