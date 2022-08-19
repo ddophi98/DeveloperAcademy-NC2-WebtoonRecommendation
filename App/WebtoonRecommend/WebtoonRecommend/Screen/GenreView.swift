49dc431e4fdab3b6ae7197e7d9059444d32d4903
ddophi98 <lightcloud98@gmail.com>
2022-08-19 16:44:46 +0900

    로티 애니메이션 추가

--- App/WebtoonRecommend/WebtoonRecommend/Screen/GenreView.swift
+++ App/WebtoonRecommend/WebtoonRecommend/Screen/GenreView.swift
@@ -8,23 +8,53 @@
 import SwiftUI
 
 struct GenreView: View {
+    @EnvironmentObject var webtoonData: WebtoonData
+    
     var body: some View {
         VStack {
-            HStack {
-                Text("장르")
-                    .foregroundColor(Color.mainText)
-                    .font(.system(size: 20, weight: .heavy))
-                Spacer()
-                Image(systemName: "magnifyingglass")
-                    .foregroundColor(Color.mainText)
-                    .font(.system(size: 20))
+            getHeaderView()
+            if webtoonData.isFinishSavingAll {
+                getContentView()
+            } else {
+                getLoadingView()
             }
-            .padding(15)
-            .background(Color.background)
-            Spacer()
         }
         .background(Color.mainText)
     }
+}
+
+// 제목과 검색 버튼 있는 헤더
+@ViewBuilder
+func getHeaderView() -> some View {
+    HStack {
+        Text("장르")
+            .foregroundColor(Color.mainText)
+            .font(.system(size: 20, weight: .heavy))
+        Spacer()
+        Image(systemName: "magnifyingglass")
+            .foregroundColor(Color.mainText)
+            .font(.system(size: 20))
+    }
+    .padding(15)
+    .background(Color.background)
+}
+
+// 로티를 이용한 로딩 애니메이션
+@ViewBuilder
+func getLoadingView() -> some View {
+    Spacer()
+    LottieView(filename: "Loading")
+        .frame(width: 200, height: 200)
+    Spacer()
+}
+
+// 원래 보여줘야할 콘텐츠
+@ViewBuilder
+func getContentView() -> some View {
+    Text("finish!")
+        .padding()
+        .background(Color.background)
+    Spacer()
 }
 
 struct GenreView_Previews: PreviewProvider {
