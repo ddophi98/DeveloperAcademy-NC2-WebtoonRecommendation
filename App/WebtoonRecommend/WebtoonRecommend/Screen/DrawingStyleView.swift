ca26e4a63635de9fdebc8338eb24a8e474b43f30
ddophi98 <lightcloud98@gmail.com>
2022-08-19 15:51:46 +0900

    탭뷰 추가

---
+++ App/WebtoonRecommend/WebtoonRecommend/Screen/DrawingStyleView.swift
@@ -0,0 +1,22 @@
+//
+//  DrawingStyleView.swift
+//  WebtoonRecommend
+//
+//  Created by 김동락 on 2022/08/19.
+//
+
+import SwiftUI
+
+struct DrawingStyleView: View {
+    var body: some View {
+        VStack {
+            Text("Drawing Style View")
+        }
+    }
+}
+
+struct DrawingStyleView_Previews: PreviewProvider {
+    static var previews: some View {
+        DrawingStyleView()
+    }
+}
---
+++ App/WebtoonRecommend/WebtoonRecommend/Screen/StoryView.swift
@@ -0,0 +1,22 @@
+//
+//  StoryView.swift
+//  WebtoonRecommend
+//
+//  Created by 김동락 on 2022/08/19.
+//
+
+import SwiftUI
+
+struct StoryView: View {
+    var body: some View {
+        VStack {
+            Text("Story View")
+        }
+    }
+}
+
+struct StoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        StoryView()
+    }
+}
